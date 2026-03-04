1500d34391dfc540f4914eaf671e34f6a8ca1016
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2026-03-04 23:02:08 +0700

    vault

--- deployment/run.sh
+++ deployment/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Build pakai env dari Vault
-envconsul -config=/vault/secrets/envconsul.hcl npm run build
+./envconsul -config=/vault/secrets/envconsul.hcl npm run build
 
 # Start dev server pakai env dari Vault
-exec envconsul -config=/vault/secrets/envconsul.hcl npm run dev -- --host 0.0.0.0 --port 5173
+./envconsul -config=/vault/secrets/envconsul.hcl npm run dev -- --host 0.0.0.0 --port 5173
