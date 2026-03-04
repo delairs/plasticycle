5bc0e64bb11542c35db61fa377e6f239d63496fe
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2026-03-04 23:53:03 +0700

    vault

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-envconsul -config=/vault/secrets/envconsul.hcl npm run dev -- --host 0.0.0.0 --port 5173
+exec envconsul -config=/vault/secrets/envconsul.hcl -- \
+  sh -c 'npm run dev -- --host 0.0.0.0 --port 5173'
